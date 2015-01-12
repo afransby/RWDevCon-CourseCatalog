350b5400c350ec630873d3182c0c3a4a2765cc11
Saul Mora <saul@magicalpanda.com>
2015-01-11 22:38:24 -0800

    reset starter project

--- CourseCatalog/1-Starter/Project/Application/CourseDataSource.swift
+++ CourseCatalog/1-Starter/Project/Application/CourseDataSource.swift
@@ -20,36 +20,4 @@ class CourseDataSource: NSObject {
 
         super.init()
     }
-
-    func updateCourseDetails(completion:(Course?) -> ()) {
-        let courseID = course?.remoteID.integerValue ?? 0
-        let context = stack.mainContext
-        requestCourse(courseID) { result in
-            context.performBlock {
-                completion(result.value)
-            }
-        }
-    }
-
-    func parseJSON(json:JSONValue) -> Course?
-    {
-        return json
-                >>- _Course.decodeObjects
-                >>- mapSome
-                >>- { $0.first }
-                >>- CourseAdapter(stack: stack).adapt
-    }
-    
-    func requestCourse(id:Int, completion: (Result<Course>) -> ()) {
-        let baseURL = NSURL(string: "https://api.coursera.org/api/catalog.v1/courses")!
-        let queryString = "id=\(id)&fields=largeIcon,smallIcon,shortDescription,aboutTheCourse"
-
-        let courseResource = jsonResource("courses", .GET, .JSONNull, parseJSON)
-        let modifyRequest = { (urlRequest : NSMutableURLRequest) in
-            urlRequest.URL = NSURL(string:baseURL.absoluteString! + "?" + queryString)
-        }
-        apiRequest(modifyRequest, baseURL, courseResource, defaultFailureHandler) { course in
-            completion(Result(course))
-        }
-    }
 }
